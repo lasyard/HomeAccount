d3319b2a22c3145c2a50e5cb5f7fec51057b6782
Lasy <lasyard@yeah.net>
2020-01-07 16:08:58 +0800

    Fixed #4: Enhance the style in html window.

--- StatHtml.cpp
+++ StatHtml.cpp
@@ -22,7 +22,7 @@ void StatHtml::showIO(DataFileRW *data, const wxString &title)
 {
     m_src = "<body>";
     h2(title);
-    m_src += "<table border='1' cellspacing='0' width='100%'>";
+    m_src += "<table border=\"1\" cellspacing=\"0\" width=\"100%\">";
     m_src += "<tr>";
     m_src += "<th>" + _("Time") + "</th>";
     m_src += "<th>" + _("Income") + "</th>";
@@ -45,10 +45,10 @@ void StatHtml::showIO(DataFileRW *data, const wxString &title)
 
 void StatHtml::firstLevelRow(const wxString &label, long money, const wxString &centLabel, const wxString &cent)
 {
-    m_src += "<tr><td width='25%'>";
+    m_src += "<tr><td width=\"25%\" align=\"center\">";
     m_src += label;
     m_src += "</td><td>";
-    m_src += "<table border='0' width='100%'>";
+    m_src += "<table border=\"0\" width=\"100%\">";
     secondLevelRow("", money, centLabel, cent);
     m_src += "</table>";
     m_src += "</td></tr>";
@@ -58,14 +58,14 @@ void StatHtml::secondLevelRow(
     const wxString &label, long money, const wxString &centLabel, const wxString &cent, bool isBu)
 {
     char buf[MONEY_LEN];
-    m_src += "<tr><td width='25%'>";
+    m_src += "<tr><td width=\"25%\">";
     bu(label, isBu);
-    m_src += "</td><td width='25%' align='right'>";
+    m_src += "</td><td width=\"25%\" align=\"right\">";
     money_to_str(buf, money);
     bu(buf, isBu);
-    m_src += "</td><td width='25%' align='right'>";
+    m_src += "</td><td width=\"25%\" align=\"right\">";
     bu(centLabel, isBu);
-    m_src += "</td><td align='right'>";
+    m_src += "</td><td align=\"right\">";
     bu(cent, isBu);
     m_src += "</td></tr>";
 }
@@ -73,8 +73,8 @@ void StatHtml::secondLevelRow(
 void StatHtml::totalTableHtml(struct mtree_node *root, long no_cat_sum)
 {
     struct cat_node *cat = get_cat_node(root);
-    m_src += "<table border='1' cellspacing='0' width='100%'>";
-    m_src += "<tr><th colspan='2' bgcolor='black'><font color='white'>";
+    m_src += "<table border=\"1\" cellspacing=\"0\" width=\"100%\">";
+    m_src += "<tr><th colspan=\"2\" bgcolor=\"black\"><font color=\"white\">";
     m_src += cat->name.str;
     m_src += "</font></th></tr>";
     firstLevelRow(_("Total"), cat->total);
@@ -91,10 +91,10 @@ void StatHtml::firstCatStatHtml(struct mtree_node *root)
     for (item = root->children.first; item != NULL; item = item->next) {
         struct mtree_node *node = get_mtree_node(item);
         struct cat_node *cat = get_cat_node(node);
-        m_src += "<tr><td>";
+        m_src += "<tr><td align=\"center\">";
         m_src += cat->name.str;
         m_src += "</td><td>";
-        m_src += "<table border='0' width='100%'>";
+        m_src += "<table border=\"0\" width=\"100%\">";
         secondLevelRow(_("Total of this category"),
                        cat->total,
                        _("Percentage in total"),
@@ -124,24 +124,22 @@ void StatHtml::showIOLine(const wxString &label, long income, long outlay)
 {
     char buf[MONEY_LEN];
     m_src += "<tr>";
-    m_src += "<td>";
+    m_src += "<td align=\"center\">";
     m_src += label;
     m_src += "</td>";
-    m_src += "<td>";
+    m_src += "<td align=\"right\"><font face=\"Monospace\">";
     money_to_str(buf, income);
     m_src += buf;
-    m_src += "</td>";
-    m_src += "<td>";
+    m_src += "</font></td>";
+    m_src += "<td align=\"right\"><font face=\"Monospace\">";
     money_to_str(buf, outlay);
     m_src += buf;
-    m_src += "</td>";
-    m_src += "<td>";
+    m_src += "</font></td>";
+    m_src += "<td align=\"right\">";
     long n = income - outlay;
-    if (n < 0) {
-        m_src += "<font color='red'>";
-    } else {
-        m_src += "<font color='green'>";
-    }
+    m_src += "<font face=\"Monospace\" color=\"";
+    m_src += (n < 0) ? "red" : "green";
+    m_src += "\">";
     money_to_str(buf, n);
     m_src += buf;
     m_src += "</font>";
