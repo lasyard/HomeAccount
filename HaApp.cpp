fe523ac6bfec1e904d9ea7739c0fed3cebdd575b
Lasy <lasyard@yeah.net>
2020-01-09 23:48:15 +0800

    Add command line arg -r

--- HaApp.cpp
+++ HaApp.cpp
@@ -1,3 +1,4 @@
+#include <wx/cmdline.h>
 #include <wx/fs_arc.h>
 #include <wx/stdpaths.h>
 #include <wx/xrc/xmlres.h>
@@ -7,6 +8,8 @@
 
 IMPLEMENT_APP(HaApp)
 
+const wxString HaApp::RESOURCE_IN_RES = "r";
+
 bool HaApp::OnInit()
 {
     if (!wxApp::OnInit()) return false;
@@ -15,12 +18,40 @@ bool HaApp::OnInit()
         wxLogError("Couldn't find/load the \"ha\" catalog");
     }
     m_locale.AddCatalog("wxstd");
+    wxImage::AddHandler(new wxPNGHandler);
     wxFileSystem::AddHandler(new wxArchiveFSHandler);
     wxXmlResource::Get()->InitAllHandlers();
-    wxXmlResource::Get()->Load(wxFileName(wxStandardPaths::Get().GetResourcesDir(), "resources.xrs").GetFullPath());
-    MainFrame *frame = new MainFrame;
+    (this->*m_loadXmlResource)();
+    MainFrame *frame = new MainFrame();
     SetTopWindow(frame);
     frame->Show();
     frame->initView();
     return true;
+}
+
+void HaApp::OnInitCmdLine(wxCmdLineParser& parser)
+{
+    parser.AddSwitch(RESOURCE_IN_RES);
+    wxAppConsole::OnInitCmdLine(parser);
+}
+
+bool HaApp::OnCmdLineParsed(wxCmdLineParser& parser)
+{
+    if (!wxApp::OnCmdLineParsed(parser)) return false;
+    if (parser.Found(RESOURCE_IN_RES)) {
+        m_loadXmlResource = &HaApp::loadXrcsInRes;
+    } else {
+        m_loadXmlResource = &HaApp::loadXrsInResouceDir;
+    }
+    return true;
+}
+
+void HaApp::loadXrsInResouceDir()
+{
+    wxXmlResource::Get()->Load(wxFileName(wxStandardPaths::Get().GetResourcesDir(), "resources.xrs").GetFullPath());
+}
+
+void HaApp::loadXrcsInRes()
+{
+    wxXmlResource::Get()->LoadAllFiles("res");
 }
--- HaApp.h
+++ HaApp.h
@@ -7,9 +7,19 @@ class HaApp : public wxApp
 {
 public:
     virtual bool OnInit();
+    virtual void OnInitCmdLine(wxCmdLineParser& parser);
+    virtual bool OnCmdLineParsed(wxCmdLineParser& parser);
 
 protected:
+    static const wxString RESOURCE_IN_RES;
+
+    // This must be long lived.
     wxLocale m_locale;
+    wxString m_resDir;
+    void (HaApp::*m_loadXmlResource)();
+
+    void loadXrcsInRes();
+    void loadXrsInResouceDir();
 };
 
 DECLARE_APP(HaApp)
